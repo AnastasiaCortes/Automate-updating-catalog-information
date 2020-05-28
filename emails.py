7cec8061eb5f8828c9713e6003fb4122f81a0ce4
Anastasia Cortes <t.nastushka@hotmail.com>
2020-05-28 13:26:04 -0400

    Added function generate_error_email() which will generate email for heaith_check.py

--- emails.py
+++ emails.py
@@ -28,6 +28,18 @@ def generate_email(sender, recipient, subject, body, attachment_path):
     return message
 
 
+def generate_error_email(sender, recipient, subject, body):
+    """Creates an email without an attachement."""
+    # Basic Email formatting
+    message = email.message.EmailMessage()
+    message["From"] = sender
+    message["To"] = recipient
+    message["Subject"] = subject
+    message.set_content(body)
+
+    return message
+
+
 def send_email(message):
     """Sends the message to the configured SMTP server."""
     mail_server = smtplib.SMTP('localhost')
